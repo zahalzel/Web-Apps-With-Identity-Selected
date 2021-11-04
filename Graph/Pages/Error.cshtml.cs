d26722ef361ccbad0aa9a3af1d8a62d84d066eff
Zach Halzel <zahalzel@microsoft.com>
2021-11-04 09:36:06 -0700

    remove corrupted file

--- Graph/Pages/Error.cshtml.cs
+++ Graph/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace WebApplication1.Pages
+namespace Graph.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
