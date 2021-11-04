0511251cfa124a9fc97fddda267d7baf62fd88ee
Zach Halzel <zahalzel@microsoft.com>
2021-11-04 09:40:09 -0700

    Configure Identity Connected Service

--- Graph/Pages/Index.cshtml.cs
+++ Graph/Pages/Index.cshtml.cs
@@ -1,19 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Identity.Web;
+using System.Net;
+using Microsoft.Graph;
 
 namespace Graph.Pages
 {
-    public class IndexModel : PageModel
+    [AuthorizeForScopes(ScopeKeySection = "MicrosoftGraph:Scopes")]    public class IndexModel : PageModel
     {
+        private readonly GraphServiceClient _graphServiceClient;
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, GraphServiceClient graphServiceClient)
         {
             _logger = logger;
+            _graphServiceClient = graphServiceClient;
         }
 
-        public void OnGet()
+        public async Task OnGet()
         {
+            var user = await _graphServiceClient.Me.Request().GetAsync();
+            ViewData["GraphApiResult"] = user.DisplayName;
 
         }
     }
